f6cc4beebe68e7c45d499d75e9480b203e0ea9cb
Mica Horton <mica.e.horton@gmail.com>
2020-03-18 21:04:51 -0700

    Work based off user input

--- pirateScraper.py
+++ pirateScraper.py
@@ -1,13 +1,34 @@
+# Enter show's name and season number to get show's intro page
+print('Enter show\'s name:')
+name = input().lower()
+print('Enter season number:')
+number = input()
+
+base = 'https://ww7.123moviesfree.sc/season/{show}-season-{season}/'
+introPage = base.format(show=name,season=number)
+
+# Get episode page url from intro page
 from selenium import webdriver
-
 driver = webdriver.Chrome()
-driver.get('https://ww7.123moviesfree.sc/season/you-season-1/watching.html/?episode=8912')
+driver.get(introPage)
 
+playButton = driver.find_element_by_xpath('//a[@class="thumb mvi-cover"]')
+episodePage = (playButton.get_attribute('href'))
+driver.get(episodePage)
+
+# Get all episodes by data-Ssrver attribute, then print episode name and video URL
 episodes = driver.find_elements_by_xpath('//a[@data-server="1"]')
-
 for item in episodes:
-    print('{:<50} {:<50}'.format(item.get_attribute('innerHTML'), item.get_attribute('data-strvid')))
+    name = item.get_attribute('innerHTML')
+    url = item.get_attribute('data-strvid') 
+    print('{:<50} {:<50}'.format(name, url))
 
-    
 driver.close()
+
+'''
+Works For: You,
+
+data-server="1" --> data-strvid
+data-server="10" --> data-drive
+'''
 
