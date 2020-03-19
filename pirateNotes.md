63df8bb4dbe18cd03f8dbd535c992542898def97
Mica Horton <mica.e.horton@gmail.com>
2020-03-18 19:38:01 -0700

    Print out table with all episodes

---
+++ pirateNotes.md
@@ -0,0 +1,31 @@
+# Project Plan
+### goal:
+given show name, episode number and season, download episode
+
+## steps:
+1. get all episodes
+    - a tags direct child of div with les-content class
+    - ata-strvid attribute
+    - data server attribute
+    - by link text (didn't work not specific enough)
+2. determine which one is the episode we want
+    - sort through title attributes
+3. download that epiosde 
+    - using lr download thing?
+
+# Resources
+Selenium Docs
+https://selenium-python.readthedocs.io/locating-elements.html#locating-hyperlinks-by-link-text
+//*[@attribute = "attribute_value"]
+
+https://stackoverflow.com/questions/51082130/getting-empty-src-while-scraping/51088253
+https://stackoverflow.com/questions/52521456/empty-src-attribute-returned-with-selenium-on-python
+
+
+# Old Stuff
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver.get('https://ww3.fmovie.sc/free/you-season-1/watching.html/?episode=10565')
+elem = driver.find_element_by_id('iframe-embed')
+print(elem.get_attribute('src'))
--- pirateScraper.py
+++ pirateScraper.py
@@ -1,45 +1,13 @@
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get('https://ww7.123moviesfree.sc/season/you-season-1/watching.html/?episode=8912')
 
+episodes = driver.find_elements_by_xpath('//a[@data-server="1"]')
 
-elem = driver.find_elements_by_id('Episode 1')
-print(elem.get_attribute('outerHTML'))
-#print(elem.get_attribute('src'))
+for item in episodes:
+    print('{:<50} {:<50}'.format(item.get_attribute('innerHTML'), item.get_attribute('data-strvid')))
 
-
-'''
-PROJECT PLAN
-goal: given show name, episode number and season, download episode
-steps:
-#1 get all episodes
-    - a tags direct child of div with les-content class
-    - ata-strvid attribute
-    - data server attribute
-    - by link text
-#2 determine which one is the episode we want
-    - sort through title attributes
-#3 download that epiosde 
-    - using lr download thing?
-or start with #2? 
-
-
-for element in elem:
-    print element.get_attribute('src')
-    print element.get_attribute('class') 
-'''
-
+    
 driver.close()
 
-'''
-Resources
-Selenium Docs
-https://selenium-python.readthedocs.io/locating-elements.html#locating-hyperlinks-by-link-text
-
-https://stackoverflow.com/questions/51082130/getting-empty-src-while-scraping/51088253
-https://stackoverflow.com/questions/52521456/empty-src-attribute-returned-with-selenium-on-python
-
-'''
